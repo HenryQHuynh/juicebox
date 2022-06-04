5da1e948961f954415d2d7802dac404e32bb58fc
Henry <henryqhuynh@gmail.com>
2022-06-03 18:38:33 -0700

    FOUND THE ERROR, it was because I forgot a RETURNING *; text in the db commands for the createPost function in index.js

--- db/index.js
+++ db/index.js
@@ -4,14 +4,22 @@ const { Client } = require('pg'); // imports the pg module
 // supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-async function getAllUsers() {
+async function createUser({
+  username,
+  password,
+  name,
+  location
+}) {
   try {
-    const { rows } = await client.query(`
-      SELECT id, username, name, location, active 
-      FROM users;
-    `);
+    const { rows: [user] } = await client.query(`
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password, name, location]);
 
-    return rows;
+    // return rows;
+    return user;
   } catch (error) {
     throw error;
   }
@@ -42,26 +50,40 @@ async function updateUser(id, fields = {}) {
   }
 }
 
-async function createUser({
-  username,
-  password,
-  name,
-  location
-}) {
+async function getAllUsers() {
   try {
-    const { rows: [user] } = await client.query(`
-      INSERT INTO users(username, password, name, location) 
-      VALUES($1, $2, $3, $4) 
-      ON CONFLICT (username) DO NOTHING 
-      RETURNING *;
-    `, [username, password, name, location]);
+    const { rows } = await client.query(`
+      SELECT id, username, name, location, active 
+      FROM users;
+    `);
 
-    // return rows;
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserById(userId) {
+  try {
+    const { rows: [ user ] } = await client.query(`
+      SELECT id, username, name, location, active
+      FROM users
+      WHERE id=${ userId }
+    `);
+
+    if (!user) {
+      return null
+    }
+
+    user.posts = await getPostsByUser(userId);
+
     return user;
   } catch (error) {
     throw error;
   }
 }
+
+  
 
 async function createPost({
   authorId,
@@ -69,16 +91,17 @@ async function createPost({
   content
 }) {
   try {
-    const { rows: [post] } = await client.query(`
-    INSERT INTO posts("authorId", title, content)
-    VALUES  ($1, $2. $3)
+    const { rows: [ post ] } = await client.query(`
+      INSERT INTO posts("authorId", title, content) 
+      VALUES($1, $2, $3)
+      RETURNING *;
     `, [authorId, title, content]);
 
     return post;
   } catch (error) {
     throw error;
   }
-};
+}
 
 async function updatePost(id, fields = {}) {
   // build the set string
@@ -131,35 +154,7 @@ async function getPostsByUser(userId) {
   }
 }
 
-async function getUserById(userId) {
-  // first get the user (NOTE: Remember the query returns 
-  // (1) an object that contains 
-  // (2) a `rows` array that (in this case) will contain 
-  // (3) one object, which is our user.
-  // if it doesn't exist (if there are no `rows` or `rows.length`), return null
 
-  // if it does:
-  // delete the 'password' key from the returned object
-  // get their posts (use getPostsByUser)
-  // then add the posts to the user object with key 'posts'
-  // return the user object
-  try {
-    const { rows: [user] } = await client.query(`
-    SELECT id, username, name, location, active FROM users
-    WHERE id=${userId}
-  `);
-
-    if (!user) {
-      return null
-    }
-
-    user.posts = await getPostsByUser(userId);
-
-    return user;
-  } catch (error) {
-    throw error;
-  }
-}
 
 // and export them
 module.exports = {  
