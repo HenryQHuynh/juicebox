614427cd267f56d1d2868ca5ae6c3571d2f96332
Henry <henryqhuynh@gmail.com>
2022-06-07 21:12:21 -0700

    Taking it nice and slow with this practice to get comfortable. Will follow-up more later if there's energy

--- API/index.js
+++ API/index.js
@@ -1,15 +1,31 @@
+// npm run start:dev
+// npm run start
+
 const PORT = 3000;
 const express = require('express');
-const server = express();
+// const server = express();
+const app = express();
 
-server.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
-  console.log(req.body);
-  console.log("<_____Body Logger END_____>");
+// server.use((req, res, next) => {
+//     console.log("<____Body Logger START____>");
+//     console.log(req.body);
+//     console.log("<_____Body Logger END_____>");
 
-  next();
+//     next();
+// });
+
+app.listen(PORT, () => {
+    console.log('The server is up on port', PORT, '. Systems operational')
 });
 
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT, '. Systems operational')
-});
+
+
+app.use((req, res, next) => {
+    console.log("A request was made to /api Hello!");
+    next();
+  });
+  
+  app.get((req, res, next) => {
+    console.log("A get request was made to /api");
+    res.send({ message: "success" });
+  });
