64989afa162d8407cefa72583ab368fa6c45d011
Henry <henryqhuynh@gmail.com>
2022-06-11 12:33:07 -0700

    Adding updates tp  api/posts per Part 3 instructionals

--- api/posts.js
+++ api/posts.js
@@ -1,11 +1,52 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
+
+
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
   next(); // THIS IS DIFFERENT
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  // res.send({ message: 'under construction' });
+
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object
+
+    if (post) {
+      res.send({ post })
+    } else {
+      next({
+        name: 'MalformedPostDataError',
+        message: 'Post data was incomplete or incorrectly formatted'
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 postsRouter.get('/', async (req, res) => {
